3bd9be460fb581e624180151c565dcfbe15c4a57
Jon Bulmer <jon.bulmer@gmail.com>
2017-02-20 21:51:11 +0000

    Code for the module pattern demo
    code and basic outline for the demo.

--- main.js
+++ main.js
@@ -1,9 +1,14 @@
 var ServiceBook = require('./serviceBook');
+var Repo = require('./carServiceRepository');
 
-var car1 = new ServiceBook('Mr H Wells','E12');
-var car2 = new ServiceBook('Mrs B Garce','G5');
-var car3 = new ServiceBook('Mr R Dellman','M5');
-var car4 = new ServiceBook('Dr V Johnson','M4');
+var car1 = new ServiceBook(Repo.get(1));
+
+var car2 = new ServiceBook({name: 'Mrs B Garce',registration: 'G5'});
+var car3 = new ServiceBook({name: 'Mr R Dellman',registration: 'M5'});
+var car4 = new ServiceBook({name: 'Dr V Johnson',registration: 'M4'});
 
 car1.save();
 car1.complete();
+car2.save();
+car3.save();
+car4.save();
--- serviceBook.js
+++ serviceBook.js
@@ -1,16 +1,19 @@
-var ServiceBook = function(name,registration) {
-    this.name = name;
-    this.registration = registration;
+var Repo = require('./carServiceRepository')
+
+var ServiceBook = function(data) {
+    this.name = data.name;
+    this.registration = data.registration;
     this.completed = false;
 }
 
-ServiceBook.prototype.complete = function() {
+ServiceBook.prototype.complete = function () {
     console.log('Job Done for ' + this.name);
     this.completed = true;
 }
 
-ServiceBook.prototype.save = function() {
-    console.log('saving Book for ' + this.name + ' car reg ' + this.registration);    
+ServiceBook.prototype.save = function () {
+    console.log('saving Book for ' + this.name + ' car reg ' + this.registration);  
+    Repo.save(this);
 }
 
 module.exports = ServiceBook;
